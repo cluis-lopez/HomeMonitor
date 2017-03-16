f5821639c715cea7f9bbedb97641f1dc5f49636d
cluis-lopez <cluis.lopez@gmail.com>
2017-03-16 02:07:36 +0100

    Python Mov_Detection syntactically corrected

--- HomeMonitor/Raspi Python Scripts/Mov_Detection.py
+++ HomeMonitor/Raspi Python Scripts/Mov_Detection.py
@@ -2,11 +2,12 @@
 
 import cv2
 import sys
+import os
 
 def compare(foto1, foto2):
-	img1 = cv2.imread(foto1)
-	foto = cv2.imread(foto2)
-	img2 = cv2.copy(foto)
+	img1 = cv2.imread(foto1, cv2.IMREAD_COLOR)
+	foto = cv2.imread(foto2, cv2.IMREAD_COLOR)
+	img2 = foto
 
 	img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 	img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -20,29 +21,43 @@ def compare(foto1, foto2):
 
 	(contornos, _) = cv2.findContours(thres.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	
-	print "Contornos detectados:" , contornos
+	numdiff = 0
 	
-	for c in contornos:
-		if cv2.contourArea(c) < 2000:
-			continue
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(foto, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	print "Numero de contornos: ", len(contornos)
 	
-
-	if contornos == None:
-		return False, 0
+	if len(contornos) == 0:
+		return 0, 0
 	else:
-		return True, foto
+		for c in contornos:
+			print "Contorno: " , contornos.index(c), " area:", cv2.contourArea(c)
+			if cv2.contourArea(c) < 2000:
+				continue
+			(x, y, w, h) = cv2.boundingRect(c)
+			cv2.rectangle(foto, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			numdiff = numdiff + 1
+		return numdiff, foto
+
 
 
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
-		print "Usage Mov_Detection <1st image file> <2nd image file>"
-		break
-	f1 = open(sys.argv[1], r);
-	f2 = open(sys.argv[2], r);
-	result, image = compare (f1, f2)
+		print "Usage: Mov_Detection <1st image file> <2nd image file>"
+		exit()
+
+	result, image = compare (sys.argv[1], sys.argv[2])
+	print "Numero de diferencias: ", result
 	
-	
+	if result == 0:
+		exit()
+
+	try:
+		if os.environ["DISPLAY"] != '':
+			cv2.imshow('Diferencias',image)
+			key = cv2.waitKey(0)
+			cv2.destroyAllWindows()
+	except:
+		print "No hay DISPLAY para mostrar la imagen"
+		
+		
 		
