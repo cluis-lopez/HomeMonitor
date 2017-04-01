24b0fa40dfad08493a2b7779e57f44df42ab1347
cluis-lopez <cluis.lopez@gmail.com>
2017-04-02 01:25:10 +0200

    Alerts added (2nd. phase)

---
+++ HomeMonitor/Raspi Python Scripts/Properties.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Sample properties file. Edit and modify with your own values
+
+URL=<your app engine URL here>
+NUMSAMPLES=<the number of env. samples taken and stored in the Raspi before send to Google>
+DELAY=<delay in seconds between samples>
+NUMPICS=<number of pictures stored both in the Raspi and in Google Storage>
