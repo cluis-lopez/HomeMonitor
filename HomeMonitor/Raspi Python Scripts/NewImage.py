ceabe3089cb9040acb8363285c72e4cc7b15ce86
cluis-lopez <cluis.lopez@gmail.com>
2017-04-04 01:07:41 +0200

    NewImage not valid anymore

--- HomeMonitor/Raspi Python Scripts/NewImage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-
-import requests
-
-r = requests.post('http://1-dot-homemonitor-156618.appspot.com/NewUpload',
-                  files={'images/00image.jpg': open('images/00image.jpg', 'rb')});
-print r
