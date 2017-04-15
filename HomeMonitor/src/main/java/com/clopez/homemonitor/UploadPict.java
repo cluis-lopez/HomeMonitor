836afc43e1a1cc223f01df7bc93edfcd36863e5b
cluis-lopez <cluis.lopez@gmail.com>
2017-04-16 01:48:50 +0200

    Minor changes to UploadPict

--- HomeMonitor/src/main/java/com/clopez/homemonitor/UploadPict.java
+++ HomeMonitor/src/main/java/com/clopez/homemonitor/UploadPict.java
@@ -65,7 +65,7 @@ public class UploadPict extends HttpServlet {
 					res.setStatus(HttpServletResponse.SC_CREATED);
 					res.setContentType("text/plain");
 					PrintWriter out = res.getWriter();
-					out.write("");
+					out.write("http://" + bucket + "/" + name);
 					out.flush();
 					out.close();
 				}
